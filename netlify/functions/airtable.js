ff4be843fec77115111fe647369506abd6f1bed7
Karyna Winner <karynawinner@gmail.com>
2026-05-08 00:56:36 +0200

    Update airtable.js

--- netlify/functions/airtable.js
+++ netlify/functions/airtable.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
- 
 exports.handler = async function(event, context) {
   const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
   const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
